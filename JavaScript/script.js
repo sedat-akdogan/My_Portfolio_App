9762251c2c3b2386a154a105ff32ba2ceec109e2
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-02 03:15:48 +0000

    dayinight buttons functions properly

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,8 @@
 // Typing Animation
 
-var typed = new Typing(".typing", {
+const typing = new TypeIt(".typing", {
     strings: ["", "Web Developer", "", "Coder", "", "Front End Developer"],
     typeSpeed: 100,
     BackSpeed:60,
     loop: true,
-});
+})
