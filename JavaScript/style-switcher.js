dafffc12f2a6a16b370c7e3c07dd3c13bbe2bae6
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-02 02:27:50 +0000

    text animation is completed

--- JavaScript/style-switcher.js
+++ JavaScript/style-switcher.js
@@ -9,7 +9,7 @@ styleSwitcherToggle.addEventListener("click", () => {
 // hide style switcher on scroll
 
 window.addEventListener("scroll", () => {
-    if(document.querySelector(".style-switcher").classList.contains("open"))
+    if (document.querySelector(".style-switcher").classList.contains("open"))
     {
         document.querySelector(".style-switcher").classList.remove("open");
     }
@@ -22,7 +22,7 @@ const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color)
 {
     alternateStyles.forEach((style) => {
-        if(color === style.getAttribute("title"))
+        if (color === style.getAttribute("title"))
         {
             style.removeAttribute("disabled");
         }
@@ -32,3 +32,23 @@ function setActiveStyle(color)
         }
     })
 }
+
+
+// Day - Night Modes
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
