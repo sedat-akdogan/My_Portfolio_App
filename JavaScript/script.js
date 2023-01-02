bd431911476a0768e61ab3d72429d8cdef790d9d
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-02 02:27:26 +0000

    JS  style-switcher section is completed

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+// Typing Animation
+
+var typed = new Typing(".typing", {
+    strings: ["", "Web Developer", "", "Coder", "", "Front End Developer"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop: true,
+});
