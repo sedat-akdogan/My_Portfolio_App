eb09195e6299173bc834a18211b4f964373306d5
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-02 00:59:58 +0000

    select colors js is completed

---
+++ JavaScript/style-switcher.js
@@ -0,0 +1,34 @@
+// toogle style switcher
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+
+// hide style switcher on scroll
+
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+
+// theme colors
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else 
+        {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
