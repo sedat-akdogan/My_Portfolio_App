acf7fe46a0d18aa4692f1351c4e3fe0095932258
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-03 15:32:33 +0000

    JS aside toggler button added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -87,3 +87,20 @@ document.querySelector(".hire-me").addEventListener("click", function()
     addBackSection(sectionIndex);
 });
 
+// aside toogler button
+
+const navTogglerBtn = document.querySelector(".nav-toggler");
+aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+})
+
+function asideSectionTogglerBtn()
+{
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for(let i=0; i<totalSection; i++)
+    {
+        allSection[i].classList.toggle("open");
+    }
+}
