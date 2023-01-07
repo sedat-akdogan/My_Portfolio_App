d718513735cc59119449afb1c15552decb5d8459
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-07 00:16:23 +0000

    JS opentab function is completed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -139,6 +139,8 @@ function opentab(tabname) {
         tablink.classList.remoce("active-link");
     }
     for(tabcontent of tabcontents) {
-        tabcontent.classList.remoce("active-link");
+        tabcontent.classList.remoce("active-tab");
     }
-}
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+} 
